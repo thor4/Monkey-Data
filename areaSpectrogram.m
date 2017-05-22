b9afaa36ac59bdecc30a8cb2ce407923e4a57230
Bryan Conklin <bryan@gozylo.com>
2017-05-22 10:05:38 -0400

    add area-resp spectrogram

--- areaSpectrogram.m
+++ areaSpectrogram.m
@@ -1,67 +1,69 @@
-movingwin=[0.5 0.05]; % set the moving window dimensions
+movingwin=[0.5 0.05]; % set the moving window dimensions, window=500ms step=50ms, w=10Hz
 params.Fs=1000; % sampling frequency
 params.fpass=[0 60]; % frequency of interest
 params.tapers=[5 9]; % tapers
 params.trialave=1; % average over trials
 params.err=0; % no error computation
 %
+min = -5;
+max = 26;
 tic
 % maxDb=25; %Limit power range to 15 Db
 [P,T,F]=mtspecgramc(Correct.area9L,movingwin,params);
 
 figure %not in chapter
 subplot(2,6,1)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal 9L'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.area8B,movingwin,params);
 subplot(2,6,2)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal 8B'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.area6DR,movingwin,params);
 subplot(2,6,3)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal 6DR'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.area8AD,movingwin,params);
 subplot(2,6,4)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal 8AD'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areavPFC,movingwin,params);
 subplot(2,6,5)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal vPFC'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areadPFC,movingwin,params);
 subplot(2,6,6)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Frontal dPFC'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areaLIP,movingwin,params);
 subplot(2,6,7)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Parietal LIP'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areaMIP,movingwin,params);
 subplot(2,6,8)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Parietal MIP'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areaPE,movingwin,params);
 subplot(2,6,9)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Parietal PE'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areaPG,movingwin,params);
 subplot(2,6,10)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Parietal PG'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 [P,T,F]=mtspecgramc(Correct.areaPEC,movingwin,params);
 subplot(2,6,11)
-imagesc(T,F,10*log10(P')) %Plot power in dB
+imagesc(T,F,10*log10(P'),[min,max]) %Plot power in dB
 axis xy; title('Parietal PEC'); xlabel('Time(s)'); ylabel('Freq (Hz)'); colormap jet; colorbar;
 
 toc
