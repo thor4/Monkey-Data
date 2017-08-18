d7e27ab400b2bd79dcf479f3524ad166df27131c
Bryan Conklin <bconkli4@fau.edu>
2017-08-18 17:46:07 -0400

    extract baseline power with BSMART

--- spectral analysis/spec-bsmart/spec-bsmart.m
+++ spectral analysis/spec-bsmart/spec-bsmart.m
@@ -2,15 +2,20 @@ load('C:\Users\bryan\OneDrive\Documents\PhD @ FAU\research\High Frequency FP Act
 load('C:\Users\bryan\OneDrive\Documents\PhD @ FAU\research\High Frequency FP Activity in VWM\data\correct.mat')
 porder = 10;
 fs = 1000;
-arpower_i = zeros(137078,501);
+arpower_cBase = zeros(352967,501);
 tic
-for i = 1:size(incorrect,2)
-    x = incorrect(:,i);
+for i = 1:size(correct,2)
+    x = correct(:,i);
     x = transpose(x);
     Nr = size(x,1);
     Nl = size(x,2);
     pp = arpower(x,Nr,Nl,porder,fs);
-    arpower_i(i,:) = pp(1,:);
+    arpower_cBase(i,:) = pp(1,:);
 end
 toc
 
+%only interested in 0-250Hz
+arpower_i = arpower_i(:,1:251);
+arpower_c = arpower_c(:,1:251);
+arpower_iBase = arpower_iBase(:,1:251);
+arpower_cBase = arpower_cBase(:,1:251);
