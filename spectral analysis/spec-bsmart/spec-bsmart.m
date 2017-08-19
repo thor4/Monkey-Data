e42a59f470ccd42c5302bb9aa095de760f01ec11
Bryan Conklin <bconkli4@fau.edu>
2017-08-19 16:57:44 -0400

    bsmart baseline-normalized power spectrum + avg

--- spectral analysis/spec-bsmart/spec-bsmart.m
+++ spectral analysis/spec-bsmart/spec-bsmart.m
@@ -4,8 +4,8 @@ porder = 10;
 fs = 1000;
 arpower_cBase = zeros(352967,501);
 tic
-for i = 1:size(correct,2)
-    x = correct(:,i);
+for i = 1:size(correctBase,2)
+    x = correctBase(:,i);
     x = transpose(x);
     Nr = size(x,1);
     Nl = size(x,2);
@@ -15,7 +15,13 @@ end
 toc
 
 %only interested in 0-250Hz
-arpower_i = arpower_i(:,1:251);
-arpower_c = arpower_c(:,1:251);
 arpower_iBase = arpower_iBase(:,1:251);
 arpower_cBase = arpower_cBase(:,1:251);
+
+%baseline normalization
+arpower_cNorm = arpower_cDelay ./ arpower_cBase;
+arpower_iNorm = arpower_iDelay ./ arpower_iBase;
+
+%average across trials
+arpower_iNormAvg = mean(arpower_iNorm);
+arpower_cNormAvg = mean(arpower_cNorm);
