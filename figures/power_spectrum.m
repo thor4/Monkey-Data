650ce8bca15e939f9868d97147a3220997c83931
Bryan Conklin <bconkli4@fau.edu>
2017-08-18 20:53:00 -0400

    power spectrum plot bins

--- figures/power_spectrum.m
+++ figures/power_spectrum.m
@@ -1,49 +1,57 @@
 %power spectrum
-doc plot_vector
+% doc plot_vector
+% figure
+% subplot(2,1,1)
+% plot_vector(ScDelay(:,10),fcDelay,'n',[],'b')
+% hold on
+% fill(theta,ScDelay(thetaIdx(1):thetaIdx(end),10),'r');
+% hold on
+% fill(fcDelay(1):fcDelay(5),ScDelay(1,10):ScDelay(length(fcDelay(1):fcDelay(5)),10),'r')
+% subplot(2,1,2)
+% plot_vector(SiDelay(:,10),fiDelay,'n',[],'g')
+deltaIdx = find(f>=0 & f<4);
+thetaIdx = find(f>=4 & f<8);
+alphaIdx = find(f>=8 & f<12);
+betaIdx = find(f>=12 & f<30);
+lowgammaIdx = find(f>=30 & f<=50);
+highgamma1Idx = find(f>=70 & f<=110);
+highgamma2Idx = find(f>=130 & f<=170);
+highgamma3Idx = find(f>=190 & f<=230);
+
+%correct power spectrum
 figure
-subplot(2,1,1)
-plot_vector(ScDelay(:,10),fcDelay,'n',[],'b')
+subplot(1,2,1);
+area(f(deltaIdx(1):deltaIdx(end)),ScNorm(deltaIdx(1):deltaIdx(end)), 'FaceColor', [.75 0 0]);
 hold on
-fill(theta,ScDelay(thetaIdx(1):thetaIdx(end),10),'r');
+area(f(deltaIdx(end):thetaIdx(end)),ScNorm(deltaIdx(end):thetaIdx(end)), 'FaceColor', [0 .75 0]);
 hold on
-fill(fcDelay(1):fcDelay(5),ScDelay(1,10):ScDelay(length(fcDelay(1):fcDelay(5)),10),'r')
-subplot(2,1,2)
-plot_vector(SiDelay(:,10),fiDelay,'n',[],'g')
-delta = fcDelay(fcDelay>0 & fcDelay<4);
-theta = fcDelay(fcDelay>4 & fcDelay<8);
-alpha = 8:12;
-beta = 12:30;
-low_gamma = 30:55;
-high_gamma1 = 65:115;
-high_gamma2 = 125:175;
-high_gamma3 = 185:235;
-
-freq = [ScDelay(deltaIdx(1):deltaIdx(end),10); ScDelay(deltaIdx(end):thetaIdx(end),10)];
-h = area(fcDelay(deltaIdx(1):deltaIdx(end)),freq);
-
-figure
-h = area(fcDelay(deltaIdx(1):deltaIdx(end)),ScDelay(deltaIdx(1):deltaIdx(end),10));
+area(f(thetaIdx(end):alphaIdx(end)),ScNorm(thetaIdx(end):alphaIdx(end)), 'FaceColor', [0 0 .75]);
 hold on
-area(fcDelay(deltaIdx(end):thetaIdx(end)),ScDelay(deltaIdx(end):thetaIdx(end),10));
-plot(fcDelay,ScDelay(:,10));
+area(f(alphaIdx(end):betaIdx(end)),ScNorm(alphaIdx(end):betaIdx(end)), 'FaceColor', [0.5 0 0]);
+hold on
+area(f(betaIdx(end):lowgammaIdx(end)),ScNorm(betaIdx(end):lowgammaIdx(end)), 'FaceColor', [0 .5 0]);
+hold on
+area(f(highgamma1Idx(1):highgamma1Idx(end)),ScNorm(highgamma1Idx(1):highgamma1Idx(end)), 'FaceColor', [0 0 .5]);
+hold on
+area(f(highgamma2Idx(1):highgamma2Idx(end)),ScNorm(highgamma2Idx(1):highgamma2Idx(end)), 'FaceColor', [.25 0 0]);
+hold on
+area(f(highgamma3Idx(1):highgamma3Idx(end)),ScNorm(highgamma3Idx(1):highgamma3Idx(end)), 'FaceColor', [0 .25 0]);
 
-Y = [1, 5, 3;
-     3, 2, 7;
-     1, 5, 3;
-     2, 6, 1];
-z = area(Y,'LineStyle',':');
+%incorrect power spectrum
+subplot(1,2,2);
+area(f(deltaIdx(1):deltaIdx(end)),SiNorm(deltaIdx(1):deltaIdx(end)), 'FaceColor', [.75 0 0]);
+hold on
+area(f(deltaIdx(end):thetaIdx(end)),SiNorm(deltaIdx(end):thetaIdx(end)), 'FaceColor', [0 .75 0]);
+hold on
+area(f(thetaIdx(end):alphaIdx(end)),SiNorm(thetaIdx(end):alphaIdx(end)), 'FaceColor', [0 0 .75]);
+hold on
+area(f(alphaIdx(end):betaIdx(end)),SiNorm(alphaIdx(end):betaIdx(end)), 'FaceColor', [0.5 0 0]);
+hold on
+area(f(betaIdx(end):lowgammaIdx(end)),SiNorm(betaIdx(end):lowgammaIdx(end)), 'FaceColor', [0 .5 0]);
+hold on
+area(f(highgamma1Idx(1):highgamma1Idx(end)),SiNorm(highgamma1Idx(1):highgamma1Idx(end)), 'FaceColor', [0 0 .5]);
+hold on
+area(f(highgamma2Idx(1):highgamma2Idx(end)),SiNorm(highgamma2Idx(1):highgamma2Idx(end)), 'FaceColor', [.25 0 0]);
+hold on
+area(f(highgamma3Idx(1):highgamma3Idx(end)),SiNorm(highgamma3Idx(1):highgamma3Idx(end)), 'FaceColor', [0 .25 0]);
 
-figure
-%fill(theta,ScDelay(thetaIdx(1):thetaIdx(end),10),'r');
-area(theta,ScDelay(thetaIdx(1):thetaIdx(end),10),'r');
-patch(theta,ScDelay(thetaIdx(1):thetaIdx(end),10),'r');
-
-deltaIdx = find(fcDelay>=0 & fcDelay<4);
-thetaIdx = find(fcDelay>=4 & fcDelay<8);
-
-x = (-10:0.1:10);
-xs = x(x>-4 & x<4);
-figure;
-hold on;
-area(xs,normpdf(xs,0,3));
-plot(x,normpdf(x,0,3));
