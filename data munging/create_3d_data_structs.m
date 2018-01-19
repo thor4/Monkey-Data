044b42c40c2e87b24a9459109a41d6d9d89834f3
thor4 <bconkli4@fau.edu>
2018-01-19 12:24:30 -0500

    update to include stable trials

--- data munging/create_3d_data_structs.m
+++ data munging/create_3d_data_structs.m
@@ -8,14 +8,14 @@ clark = { days_clark, "session02", "session03" };
 % data = zeros(25,7000,2000); %delay period total
 days_betty_num = [90615, 90616, 90617, 90618, 90622, 90625, 90626, 90629, 90701, 90702, 90706, 90708, 90709, 90901, 90903, 90916, 90917, 90921, 90923, 90924, 90928, 90929, 90930, 91001];
 days_clark_num = [60328, 60406, 60411, 60414, 60426, 60427, 60428, 60502, 60503, 60509, 60511, 60531, 60601, 60602, 60824, 60825, 60831, 60907, 61212, 61213, 61214, 61215, 61221];
-trial_path = 'D:\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\trial_info.mat';
-recording_path = 'D:\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\recording_info.mat';
-trial_lfp_path = 'D:\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\%s%s%s.%04d.mat';
+trial_path = 'C:\\Users\\bryan\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\trial_info.mat';
+recording_path = 'C:\\Users\\bryan\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\recording_info.mat';
+trial_lfp_path = 'C:\\Users\\bryan\\OneDrive\\Documents\\PhD @ FAU\\research\\High Frequency FP Activity in VWM\\%s\\%s\\%s\\%s%s%s.%04d.mat';
 % trial_path = '//home//bconkli4//Documents//data//original//%s//%s//%s//trial_info.mat';
 % recording_path = '//home//bconkli4//Documents//data//original//%s//%s//%s//recording_info.mat';
 % trial_lfp_path = '//home//bconkli4//Documents//data//original//%s//%s//%s//%s%s%s.%04d.mat';
 areas = ["9L", "8B", "6DR", "8AD", "vPFC", "dPFC", "LIP", "MIP", "PE", "PG", "PEC"];
-trial_length = 2100; %in ms, found in short-trial search
+% trial_length = 2100; %in ms, found in short-trial search
 
 tic
 %betty loop
@@ -27,35 +27,40 @@ for i=1:length(betty{1})
     %exploring trial lengths    
     day = sprintf('d%s',days_betty(i));
     z = 1;
-%     for j=1:trial_info.numTrials
-%         if (trial_info.good_trials(j) == 1) 
-%               trial_lfp_myfilename = sprintf(trial_lfp_path, monkeys(1), betty{1}{i}, betty{2}, monkeys(1), betty{1}{i}, betty{2}{1}(8:9), j);
-%               load(trial_lfp_myfilename);
-%               trial = sprintf('t%d',j);
-%               [r,c] = size(lfp_data);
-%     %         bTrialLength(j,1) = c;
-%               lfp_data_mv = lfp_data .* 1000000; % convert to µV (1V = 10^6µV = 1,000,000µV)
-%     %         data(1:r,1:trial_length,j) = lfp_data_mv(:,1:trial_length); %these columns don't dynamically update, they just grow so there are extra 0's. can't remove all 0's since some are signal
-% %             bettyData.(day).(trial) = lfp_data_mv;
-% %             clear lfp_data_mv
-%               bettyGoodTrials.(day).(trial) = lfp_data_mv(:,trial_info.CueOnset(j)-500:end); %store shifted time series with 500ms prestimulus baseline as start
-%               offset = c - size(bettyGoodTrials.(day).(trial),2); %calc offset for revised markers
-%               new_trial_info.TrialNum(z) = j;
-%               new_trial_info.CueOnset(z) = trial_info.CueOnset(j) - offset; %0 is now a point, so new CueOnset will be 501 for all shifted trials
-%               new_trial_info.CueOffset(z) = trial_info.CueOffset(j) - offset; %should be 1001-1051 for 500-550 ms sample window
-%               new_trial_info.MatchOnset(z) = trial_info.MatchOnset(j) - offset; 
-%               new_trial_info.BehResp(z) = trial_info.BehResp(j);
-%               new_trial_info.Rule(z) = trial_info.rule(j);
-%               z = z + 1;
-%               %need to determine the distribution of sample-size windows, do a
-%               %histogram of (CueOffset - CueOnset) values and see for one
-%               %day. ask bressler how you do a spectrogram incl baseline and
-%               %sample in addition to delay if you have so many variable
-%               %window sizes, of which sample is just one
-%         end
-%     end
-%     bettyGoodTrials.(day).new_trial_info = new_trial_info;
-    bettyGoodTrials.(day).recording_info = recording_info;
+    for j=1:trial_info.numTrials
+        if (trial_info.good_trials(j) == 1) && (trial_info.stable_trials(j) == 1)
+              trial_lfp_myfilename = sprintf(trial_lfp_path, monkeys(1), betty{1}{i}, betty{2}, monkeys(1), betty{1}{i}, betty{2}{1}(8:9), j);
+              load(trial_lfp_myfilename);
+              trial = sprintf('t%d',j);
+              [r,c] = size(lfp_data);
+    %         bTrialLength(j,1) = c;
+              lfp_data_mv = lfp_data .* 1000000; % convert to µV (1V = 10^6µV = 1,000,000µV)
+    %         data(1:r,1:trial_length,j) = lfp_data_mv(:,1:trial_length); %these columns don't dynamically update, they just grow so there are extra 0's. can't remove all 0's since some are signal
+%             bettyData.(day).(trial) = lfp_data_mv;
+%             clear lfp_data_mv
+              bettyGoodStableTrials.(day).(trial) = lfp_data_mv(:,trial_info.CueOnset(j)-500:end); %store shifted time series with 500ms prestimulus baseline as start
+              new_trial_info.TrialLength(z) = size(bettyGoodStableTrials.(day).(trial),2);
+              offset = c - new_trial_info.TrialLength(z); %calc offset for revised markers
+              new_trial_info.TrialNum(z) = j;
+              new_trial_info.CueOnset(z) = trial_info.CueOnset(j) - offset; %0 is now a point, so new CueOnset will be 501 for all shifted trials
+              new_trial_info.CueOffset(z) = trial_info.CueOffset(j) - offset; %should be 1001-1051 for 500-550 ms sample window
+              new_trial_info.MatchOnset(z) = trial_info.MatchOnset(j) - offset; 
+              new_trial_info.BehResp(z) = trial_info.BehResp(j); new_trial_info.Rule(z) = trial_info.rule(j);
+              new_trial_info.CueObj(z) = trial_info.CueObj(j); new_trial_info.CueLoc(z) = trial_info.CueLoc(j);
+              new_trial_info.MatchObj1(z) = trial_info.MatchObj1(j); new_trial_info.MatchObj2(z) = trial_info.MatchObj2(j);
+              new_trial_info.MatchPos1(z) = trial_info.MatchPos1(j); new_trial_info.MatchPos2(z) = trial_info.MatchPos2(j);
+              new_trial_info.FirstSac(z) = trial_info.FirstSac(j);
+              z = z + 1;
+              clear lfp_data_mv
+              %need to determine the distribution of sample-size windows, do a
+              %histogram of (CueOffset - CueOnset) values and see for one
+              %day. ask bressler how you do a spectrogram incl baseline and
+              %sample in addition to delay if you have so many variable
+              %window sizes, of which sample is just one
+        end
+    end
+    bettyGoodStableTrials.(day).new_trial_info = new_trial_info;
+    bettyGoodStableTrials.(day).recording_info = recording_info;
     %creating cell within a struct for each recording session's data
 %     bettyTrials.(day) = trial_info.TrialLength;
 %     bettyData.(day) = data;
@@ -63,6 +68,7 @@ for i=1:length(betty{1})
 %     trial_info.TrialLength = bTrialLength;
 %     save(trial_myfilename, 'trial_info');
 %     clear bTrialLength
+    clear new_trial_info
     clear recording_info
 end
 toc
@@ -79,45 +85,45 @@ for i=1:length(clark{1})
         load(trial_myfilename);
         recording_myfilename = sprintf(recording_path, monkeys(2), clark{1}{i}, clark{j});
         load(recording_myfilename);
-%         for k=1:trial_info.numTrials
-%             if (trial_info.good_trials(k) == 1)
-%               trial_lfp_myfilename = sprintf(trial_lfp_path,  monkeys(2), clark{1}{i}, clark{j}, monkeys(2), clark{1}{i}, clark{j}{1}(8:9), k);
-%               load(trial_lfp_myfilename);
-%               trial = sprintf('t%d',k);
-%               [r,c] = size(lfp_data);
-% %             cTrialLength(k,1) = c;
-%               lfp_data_mv = lfp_data .* 1000000; % convert to µV (1V = 10^6µV = 1,000,000µV)
-% %             delay_period = trial_info.MatchOnset(k)-trial_info.CueOffset(k);
-% %                 for l=1:r % parse through each channel
-% %                 % insert voltage values during delay period, take one
-% %                 % second after, up to 810ms after
-% %                 % Pull out electrode in a specific recording area
-% %                     if (recording_info.area(l) == area)
-% %                         data(1:810,idx) = lfp_data_mv(l,floor(trial_info.CueOffset(k))+1:floor(trial_info.CueOffset(k))+810); %delay
-% %                         %data(1:400,idx) = lfp_data_mv(l,floor(trial_info.CueOnset(k))-450:floor(trial_info.CueOnset(k))-51); %baseline
-% %                         idx = idx + 1;
-% %                     end
-% %                 end
-% %                 end
-%               clarkGoodTrials.(day).(sesh).(trial) = lfp_data_mv(:,floor(trial_info.CueOnset(k)-500):end); %store shifted time series with 500ms prestimulus baseline as start
-%               offset = c - size(clarkGoodTrials.(day).(sesh).(trial),2); %calc offset for revised markers
-%               new_trial_info.TrialNum(z) = k;
-%               new_trial_info.CueOnset(z) = floor(trial_info.CueOnset(k) - offset); %0 is now a point, so new CueOnset will be 501 for all shifted trials
-%               new_trial_info.CueOffset(z) = floor(trial_info.CueOffset(k) - offset); %should be 1001-1051 for 500-550 ms sample window
-%               new_trial_info.MatchOnset(z) = floor(trial_info.MatchOnset(k) - offset); 
-%               new_trial_info.BehResp(z) = trial_info.BehResp(k);
-%               new_trial_info.Rule(z) = trial_info.rule(k);
-%               z = z + 1;
-%             end
-%         end
-%         clarkGoodTrials.(day).(sesh).new_trial_info = new_trial_info;
-        clarkGoodTrials.(day).(sesh).recording_info = recording_info;
-%         day = sprintf('d%s',days_clark(i));
-%         sesh = sprintf('%s%s',day,string(clark(j)));
-%         clarkTrials.(sesh) = trial_info.TrialLength;
-%         trial_info.TrialLength = cTrialLength;
-%         save(trial_myfilename, 'trial_info');
-%         clear cTrialLength
+        for k=1:trial_info.numTrials
+            if (trial_info.good_trials(k) == 1) && (trial_info.stable_trials(k) == 1)
+              trial_lfp_myfilename = sprintf(trial_lfp_path,  monkeys(2), clark{1}{i}, clark{j}, monkeys(2), clark{1}{i}, clark{j}{1}(8:9), k);
+              load(trial_lfp_myfilename);
+              trial = sprintf('t%d',k);
+%             cTrialLength(k,1) = c;
+              lfp_data_mv = lfp_data .* 1000000; % convert to µV (1V = 10^6µV = 1,000,000µV)
+              [r,c] = size(lfp_data);
+%             delay_period = trial_info.MatchOnset(k)-trial_info.CueOffset(k);
+%                 for l=1:r % parse through each channel
+%                 % insert voltage values during delay period, take one
+%                 % second after, up to 810ms after
+%                 % Pull out electrode in a specific recording area
+%                     if (recording_info.area(l) == area)
+%                         data(1:810,idx) = lfp_data_mv(l,floor(trial_info.CueOffset(k))+1:floor(trial_info.CueOffset(k))+810); %delay
+%                         %data(1:400,idx) = lfp_data_mv(l,floor(trial_info.CueOnset(k))-450:floor(trial_info.CueOnset(k))-51); %baseline
+%                         idx = idx + 1;
+%                     end
+%                 end
+%                 end
+              clarkGoodStableTrials.(day).(sesh).(trial) = lfp_data_mv(:,floor(trial_info.CueOnset(k)-500):end); %store shifted time series with 500ms prestimulus baseline as start
+              new_trial_info.TrialLength(z) = size(clarkGoodStableTrials.(day).(sesh).(trial),2);
+              offset = c - new_trial_info.TrialLength(z); %calc offset for revised markers
+              new_trial_info.TrialNum(z) = k;
+              new_trial_info.CueOnset(z) = floor(trial_info.CueOnset(k) - offset); %0 is now a point, so new CueOnset will be 501 for all shifted trials
+              new_trial_info.CueOffset(z) = floor(trial_info.CueOffset(k) - offset); %should be 1001-1051 for 500-550 ms sample window
+              new_trial_info.MatchOnset(z) = floor(trial_info.MatchOnset(k) - offset); 
+              new_trial_info.BehResp(z) = trial_info.BehResp(k); new_trial_info.Rule(z) = trial_info.rule(k);
+              new_trial_info.CueObj(z) = trial_info.CueObj(k); new_trial_info.CueLoc(z) = trial_info.CueLoc(k);
+              new_trial_info.MatchObj1(z) = trial_info.MatchObj1(k); new_trial_info.MatchObj2(z) = trial_info.MatchObj2(k);
+              new_trial_info.MatchPos1(z) = trial_info.MatchPos1(k); new_trial_info.MatchPos2(z) = trial_info.MatchPos2(k);
+              new_trial_info.FirstSac(z) = trial_info.FirstSac(k);
+              z = z + 1;
+              clear lfp_data_mv
+            end
+        end
+        clarkGoodStableTrials.(day).(sesh).new_trial_info = new_trial_info;
+        clarkGoodStableTrials.(day).(sesh).recording_info = recording_info;
+        clear new_trial_info
         clear recording_info
     end
 end
