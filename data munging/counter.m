eec64485170046268ffbeefb9a2d46f7775637b4
Bryan Conklin <bryan.conklin4@gmail.com>
2018-06-28 15:09:20 -0400

    update counter to include correct+incorrect

--- data munging/counter.m
+++ data munging/counter.m
@@ -1,6 +1,7 @@
+load('mGoodStableRule1-split_by_Day_BehResp_and_Chan.mat')
 %day
 d = 'd%i';
-
+%correct
 for i=1:numel(monkey(1).day)
     chan = fieldnames(monkey(1).day(i).correct);
     day = sprintf(d, i);
@@ -13,4 +14,15 @@ for i=1:numel(monkey(1).day)
     end
 end
 
-monkeyNew(1).day(i).correct.(chan{j});
+%incorrect
+for i=1:numel(monkey(1).day)
+    chan = fieldnames(monkey(1).day(i).incorrect);
+    day = sprintf(d, i);
+    for j=1:numel(chan)
+        [~,pings,idx] = ping_rej(monkey(1).day(i).incorrect.(chan{j}),-750,750);
+        if (pings > 0)
+            %keep track of total number of pings per area per day
+            counts.day(i).incorrect.(day).(chan{j}) = pings;
+        end
+    end
+end
