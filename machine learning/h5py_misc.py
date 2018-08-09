38a3e3c7a37a9b23bdb1354f6493c8d3ebf2e0a4
Bryan Conklin <bryan.conklin4@gmail.com>
2018-08-09 11:16:52 -0400

    h5 testing

--- machine learning/h5py_misc.py
+++ machine learning/h5py_misc.py
@@ -7,8 +7,36 @@ Created on Fri Jul 20 10:43:40 2018
 
 import h5py
 import numpy as np
+from itertools import *
 
-f = h5py.File('mGoodStableRule1PingRej-split_by_Day_BehResp_and_Chan.mat','r')
+f = h5py.File('mGoodStableRule1PingRej.h5','r')
+
+f.keys()
+f.values()
+f.items()
+
+f.name
+for name in f:
+    print(name)
+    
+def find_chan(name):
+    #find chan in directory
+    if 'LIP' in name:
+        return name
+
+def print_attrs(name, obj):
+    print(name)
+    for key, val in obj.attrs.iteritems():
+        print("    %s: %s" % (key, val))
+
+g = f.visit(get_names)
+
+f.visititems(print_attrs)
+    
+d = {'a': 1, 'b': 2}
+dvi = f.itervalues()
+dvi.next()
+
 
 g = f['monkey/day']
 
