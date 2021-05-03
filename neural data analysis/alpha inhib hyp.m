2822390d5d509e25715dd8210470c1ad9eacc2cc
Bryan Conklin <6412103+thor4@users.noreply.github.com>
2021-05-02 22:02:16 -0400

    first run of clark power + trials by match location

--- neural data analysis/alpha inhib hyp.m
+++ neural data analysis/alpha inhib hyp.m
@@ -49,6 +49,14 @@ frex = logspace(log10(min_freq),log10(max_freq),num_frex); %total num of freq's
 %274 samples in match (check this)
 %2094 total samples across all chans, 35 total frequencies
 
+% define trial timeline
+signalt = -.504:1/srate:1.589; %505 (baseline) + 504 (nonzero sample) + 811 (delay) + 274 (match)=1589+505=2094ms
+% vector of time points to save in post-analysis downsampling
+times2save = -400:10:1466; % in ms, 1466 = 505 (sample) + 811 (delay) + 150 (match)
+% time vector converted to indices
+times2saveidx = dsearchn((signalt.*1000)',times2save');
+
+tic
 for day=alldays %cycle through all days
     for j=2:3
         if (j==3) && (monkey=="betty") %only one session for betty
@@ -58,89 +66,87 @@ for day=alldays %cycle through all days
         load(trial_infoN,'trial_info'); %load trial_info for day's trials
         recording_infoN = sprintf(recording_info_path,monkey,day{:},days{j}); %create full path to recording_info.mat
         load(recording_infoN,'recording_info'); %load recording_info for day's trials
-        areas = recording_info.area; 
+        areas = recording_info.area;
         day_ansig = sprintf(lfp_path,monkey,day{:},days{j},monkey,day{:},days{j}{1}(8:9));
         %load analytic signal for all trials across all chans in a single variable 'ansig':
         load(day_ansig,'ansig'); %chan (recording_info.numChannels) x frex (35) x time (2079) x trial (trial_info.numTrials)
         %size(ansig) %verify above
-              
-        for k=1:recording_info.numChannels %parse all channels
-            r1_locs = []; r2_locs = []; %init location vars for ea rule
-            %first, compile trials of interest
-            for l=1:trial_info.numTrials
-                if (trial_info.BehResp(l)==1) && (trial_info.rule(l)==1) %cor + rule1
-                    if (trial_info.CueObj(l)==trial_info.MatchObj1(l)) %match identity for rule 1
-                        r1_locs(end+1,:) = [l trial_info.MatchPos1(l)]; %save trial # & position
-                    elseif (trial_info.CueObj(l)==trial_info.MatchObj2(l))
-                        r1_locs(end+1,:) = [l trial_info.MatchPos2(l)];
-                    end
-                elseif (trial_info.BehResp(l)==1) && (trial_info.rule(l)==2) %cor + rule2
-                    if (trial_info.CueLoc(l)==trial_info.MatchPos1(l)) %match location for rule 2
-                        r2_locs(end+1,:) = [l trial_info.MatchPos1(l)];
-                    elseif (trial_info.CueLoc(l)==trial_info.MatchPos2(l))
-                        r2_locs(end+1,:) = [l trial_info.MatchPos2(l)];
-                    end
+        r1_locs = []; r2_locs = []; %init location vars for ea rule
+        %first, compile trials of interest
+        for l=1:trial_info.numTrials
+            if (trial_info.BehResp(l)==1) && (trial_info.rule(l)==1) %cor + rule1
+                if (trial_info.CueObj(l)==trial_info.MatchObj1(l)) %match identity for rule 1
+                    r1_locs(end+1,:) = [l trial_info.MatchPos1(l)]; %save trial # & position
+                elseif (trial_info.CueObj(l)==trial_info.MatchObj2(l))
+                    r1_locs(end+1,:) = [l trial_info.MatchPos2(l)];
+                end
+            elseif (trial_info.BehResp(l)==1) && (trial_info.rule(l)==2) %cor + rule2
+                if (trial_info.CueLoc(l)==trial_info.MatchPos1(l)) %match location for rule 2
+                    r2_locs(end+1,:) = [l trial_info.MatchPos1(l)];
+                elseif (trial_info.CueLoc(l)==trial_info.MatchPos2(l))
+                    r2_locs(end+1,:) = [l trial_info.MatchPos2(l)];
                 end
             end
-            if ~isempty(r1_locs) %ensures r1_locs isn't empty
-                r1_locs_split=[]; %init var to hold trial numbers split by location
-                all_locs = unique(r1_locs(:,2))'; %row vec of all locations
-                for location=all_locs %cycle through locations (needs to be row vec)
-                    temp_loc = sprintf('l%d',location);
-                    r1_locs_split.(temp_loc) = r1_locs(find(r1_locs(:,2)==location)); %returns trials at loc specified by 'location'
-                end
-            end
-            if ~isempty(r2_locs) %ensures r2_locs isn't empty
-                r2_locs_split=[]; %init var to hold trial numbers split by location
-                all_locs = unique(r2_locs(:,2))'; %row vec of all locations
-                for location=all_locs %cycle through locations (needs to be row vec)
-                    temp_loc = sprintf('l%d',location);
-                    r2_locs_split.(temp_loc) = r2_locs(find(r2_locs(:,2)==location)); %returns trials at loc specified by 'location'
-                end
-            end
-        %STOPPED HERE
-        %PULLED OUT SOME TRIALS OF INTEREST: COR + RULE 1/2 (sep var)
-        %NOW CONTINUE TO COMPUTE AVG POWER ACROSS TRIALS PER STIMULUS LOCATION
-            
-            
-            
-            
-            for fi=1:length(frex)
-                % & save avg power per freq component avg across trials
-                pow(fi) = mean( abs( ansig(k,fi,:,l) as.^2 ),4 );
-                %need to identify stim location based on match loc & rule
-                % store dB-norm'd down-sampled power for each frequency in freq
-                % x time x trials
-%                 dbpow(fi,:,:) = 10*log10( abs( as(times2saveidx,:) ) .^2 ./basePow); 
-                % save raw power for ea. freqidx x down-sampled time x
-                % trial
-%                 pow(fi,:,:) = abs( as(times2saveidx,:) ) .^2;
-                % mean( abs( as_ ).^2, 2);
-%                     clear as % start anew with these var's ea. loop
-                if 
-                    continue
-                elseif (trial_info.rule(l)==2) %rule2 (location)
-                    continue
-                end
-            end
-                    
-                
-            %save downsampled power as chan x freqidx x time x trials
-%             data.(monkey{:})(i).(dday{:}).power(chan,:,:,:) = pow; 
-            %save avg baseline power for all frex as chan x freqidx
-            data.(monkey{:})(i).(dday{:}).basepow(chan,:) = basePow; % as is now a time x trial complex matrix
-                ansig(k,fi,:,:) = as;
         end
-        lfp_as_path = strcat(path,'%s\\%s\\%s\\%s%s%s.ansig.mat'); %build lfp all analytic signals path
-        lfp_as = sprintf(lfp_as_path,monkey,day{:},days{j},monkey,day{:},days{j}{1}(8:9));
-        save(lfp_as,'ansig','-v7.3') %save chan x freq (35) x time (2079) x trials ansig matrix
-        clear ansig
-%         % testing:
-%         find(ansig(:,:,:,:)==0);
-%         size(ansig)
+        if ~isempty(r1_locs) %ensures r1_locs isn't empty
+            r1=[]; %init var to hold trial numbers split by match location
+            all_locs = unique(r1_locs(:,2))'; %row vec of all locations
+            for location=all_locs %cycle through locations (needs to be row vec)
+                temp_loc = sprintf('l%d_tri',location);
+                r1.(temp_loc) = r1_locs(find(r1_locs(:,2)==location)); %returns trials at loc specified by 'location'
+            end
+            for location=all_locs %cycle through locations (needs to be row vec)
+                temp_loc = sprintf('l%d_tri',location);
+                temp_loc_pow = sprintf('l%d_pow',location);
+                %init temp power var (chan x down-sampled time x frequency):
+                temp_pow = zeros(length(areas),length(times2save),length(frex));
+                for k=1:length(areas) %parse all channels
+                    for fi=1:length(frex)
+                        % save avg raw power for ea. chan x freq x down-sampled time avg across trials
+                        temp_pow(k,:,fi) = squeeze( mean( abs( ansig(k,fi,times2saveidx,r1.(temp_loc)) ) .^2 ,4) );
+                        % end up with down-sampled time vector of avg pow vals (187x1)
+                    end
+                end
+                r1.(temp_loc_pow) = temp_pow; %save chan x time x frex avg pow over tri per loc
+            end
+        end
+        if ~isempty(r2_locs) %ensures r2_locs isn't empty
+            r2=[]; %init var to hold trial numbers split by match location
+            all_locs = unique(r2_locs(:,2))'; %row vec of all locations
+            for location=all_locs %cycle through locations (needs to be row vec)
+                temp_loc = sprintf('l%d_tri',location);
+                r2.(temp_loc) = r2_locs(find(r2_locs(:,2)==location)); %returns trials at loc specified by 'location'
+            end
+            for location=all_locs %cycle through locations (needs to be row vec)
+                temp_loc = sprintf('l%d_tri',location);
+                temp_loc_pow = sprintf('l%d_pow',location);
+                %init temp power var (chan x down-sampled time x frequency):
+                temp_pow = zeros(length(areas),length(times2save),length(frex));
+                for k=1:length(areas) %parse all channels
+                    for fi=1:length(frex)
+                        % save avg raw power for ea. chan x freq x down-sampled time avg across trials
+                        temp_pow(k,:,fi) = squeeze( mean( abs( ansig(k,fi,times2saveidx,r2.(temp_loc)) ) .^2 ,4) );
+                        % end up with down-sampled time vector of avg pow vals (187x1)
+                    end
+                end
+                r2.(temp_loc_pow) = temp_pow; %save chan x time x frex avg pow over tri per loc
+            end
+        end
+        powloc_path = strcat(path,'%s\\%s\\%s\\%s%s%s.powloc.mat'); %build power x location data path
+        pow_loc = sprintf(powloc_path,monkey,day{:},days{j},monkey,day{:},days{j}{1}(8:9));
+        if exist('r1','var') && ~exist('r2','var') %only r1 in this session
+            save(pow_loc,'r1','-v7.3') %save tri & pow x location struct
+            clear r1
+        elseif exist('r2','var') && ~exist('r1','var') %only r2 in this session
+            save(pow_loc,'r2','-v7.3') %save tri & pow x location struct
+            clear r2
+        else %both r1 & r2 in this session
+            save(pow_loc,'r1','r2','-v7.3') %save tri & pow x location struct
+            clear r1 r2
+        end
     end
 end
+%all correct trials and power split by stim location are now saved in a single file per session
+toc
 
-
-
-
+% 1423.38 sec for clark
